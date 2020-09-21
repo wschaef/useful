7b768094ff704e57ac3d5483e59e06ca147a4a03
Waldemar Schäfer <mydev.ws@gmail.com>
2020-09-21 11:33:17 +0200

    Update rebuild.sh

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -2,5 +2,5 @@ DOCKERFILE_NAME=Dockerfile
 IMAGE_NAME=change-me
 docker rmi $(docker images -f dangling=true -q)
 docker rmi -f $IMAGE_NAME
-docker build -t $IMAGE_NAME -f ${DOCKERFILE_NAME} .
+docker build -t $IMAGE_NAME -f $DOCKERFILE_NAME .
 docker run -p 8000:8080 $IMAGE_NAME
