6a25747262260d718c7288cc1ef00e62a215a0d2
Waldemar Schäfer <mydev.ws@gmail.com>
2020-09-21 09:20:15 +0200

    fix spacing

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,5 +1,5 @@
-DOCKERFILE_NAME = Dockerfile
-IMAGE_NAME = "change-me"
+DOCKERFILE_NAME=Dockerfile
+IMAGE_NAME=change-me
 docker rmi $(docker images -f dangling=true -q)
 docker rmi -f $IMAGE_NAME
 docker build -t $IMAGE_NAME -f ${DOCKERFILE_NAME} .
